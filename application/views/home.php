b18c9d811b73ef7220dedac7c8ed9ac816408961
cineasthales <thalesccastro@gmail.com>
2018-05-10 21:08:29 -0300

    Mais estilizações da home

--- application/views/home.php
+++ application/views/home.php
@@ -24,32 +24,54 @@ if ($this->session->has_userdata('mensagem')) {
 }
 ?>
 <main>
-    <section>
+    <section class="fundo1">
         <div class="row">
             <div class="col-12">
-                <h1 class="homeh1">Listas de presentes personalizadas</h1>
-                <p>Organize seu evento, monte sua lista e convide seus amigos. Tudo gratuitamente. </p><br>
+                <h1 class="homeh">LISTAS DE PRESENTES PERSONALIZADAS</h1>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-6">
+                <p>Organize seu <strong>evento</strong>.<br>
+                    Monte sua <strong>lista</strong> de presentes.<br>
+                    Convide seus <strong>amigos</strong>.<br><br>
+                    <strong>Tudo gratuitamente.</strong></p>
+            </div>
+            <div class="col-6">
                 <button><a href="<?= base_url('home/cadastrar') ?>">Cadastre-se</a></button>
             </div>
         </div>
     </section>
-    <section class="fundoAlt">
+    <section class="fundo2">
         <div class="row">
-            <div class="col-12">
-                <h1 class="homeh1">Nunca mais erre o presente</h1>
+            <div class="col-6">
+                <h2 class="homeh"><i class="fas fa-gift"></i><br>Nunca mais erre o presente</h2>
                 <p>Gifty foi projetado para que você saiba exatamente que presentes
-                    escolher, sem riscos de constrangimentos ou de repetições.</p>
+                    escolher, sem constrangimentos, nem repetições.</p>
             </div>
-            <div class="col-12">
-                <br><br><h1 class="homeh1">O que você vai ganhar ainda será um segredo</h1>
+            <div class="col-6">
+                <h2 class="homeh"><i class="fas fa-user-secret"></i><br>O que você vai ganhar ainda será um segredo</h2>
                 <p>Os seus convidados marcam os itens comprados, mas você não fica
                     sabendo quem comprou e o que foi comprado.</p>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <h2 class="homeh"><i class="fas fa-search"></i><br>Mais liberdade para pesquisar</h2>
+                <p>Você e seus convidados não precisam mais ficar
+                    sujeitos às opções e aos preços de uma mesma loja.</p>
+            </div>
+            <div class="col-6">
+                <h2 class="homeh"><i class="fas fa-calendar-check"></i><br>Para qualquer ocasião particular</h2>
+                <p>Utilize o Gifty para casamento, formatura, aniversário ou qualquer
+                    outro evento particular.</p>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-12">
-                <br><br><h1 class="homeh1">Sem mais listas em lojas específicas</h1>
-                <p>Não importa se for casamento, formatura, aniversário ou qualquer
-                    outro evento particular. Você e seus convidados não precisam mais ficar
-                    sujeitos às opções de uma mesma loja.</p>
+                <button id="saibamais"><a href="<?= base_url('sobre') ?>">Saiba mais</a></button>
             </div>
         </div>
     </section>
