5d78037555ff3407e440b247f2d0800e55292591
Thales Castro <28463839+cineasthales@users.noreply.github.com>
2018-08-05 04:53:27 -0300

    Nova versão font awesome

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="<?= base_url('assets/css/normalize.css') ?>">
         <link rel="stylesheet" href="<?= base_url('assets/css/reset.css') ?>">
         <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="<?= base_url('assets/js/jquery.js') ?>"></script>        
     </head>
